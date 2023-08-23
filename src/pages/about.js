5d132e43132746d493b7dc31a9020715013b02ec
Emqubeui2 <Emqubeui2@Emqubeindia.local>
2023-08-23 16:20:56 +0530

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const About = () => {
+    return(
+        <main>
+            <h1>about me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi amet, mollitia tenetur repellendus dolore enim? Enim, unde vitae exercitationem ipsam nihil quibusdam repudiandae dolorum sint perferendis sequi quam non eius.</p>
+        </main>
+    )  
+}
+
+export default About
---
+++ src/pages/functiondemo.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const FunctionDemo = () =>{
+    return(
+      <Layout pageTitle= "Function page">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nobis quas, at repellat ea provident maxime asperiores rem tempora tenetur facilis! Repellendus earum, totam dolorem obcaecati accusamus doloribus tempore beatae?</p>
+      </Layout>
+    )
+}
+export default FunctionDemo
