dcfb27b2a5dd29933cb97fa279540719fa2c60ae
Emqubeui2 <Emqubeui2@Emqubeindia.local>
2023-08-23 17:22:31 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,11 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-smoothscroll`,
+  ],
+ 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import scrollTo from 'gatsby-plugin-smoothscroll';
 
 
 const IndexPage = () => {
   return(
+  <>
   <Layout pageTitle="Home page">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident voluptates fugiat! Eius veniam error dignissimos reiciendis nam nemo unde autem, impedit voluptas culpa adipisci maiores laborum molestiae repellendus modi!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident voluptates fugiat! Eius veniam error dignissimos reiciendis nam nemo unde autem, impedit voluptas culpa adipisci maiores laborum molestiae repellendus modi!</p>
+    <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/about-header-desk.jpg"
+      />
   </Layout>
+  <button onClick={() => scrollTo('#some-id')}>My link</button>
+  </>
   )
 }
 
